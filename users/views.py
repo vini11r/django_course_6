245814b1550b3f61e17ea0a8c7249391ce4ef166
Pavel Gornostaev <pavelgornostaeff@yandex.ru>
2024-07-22 23:16:53 +0300

    регистрация пользователя

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import UserRegisterForm
+from users.models import User
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
+
