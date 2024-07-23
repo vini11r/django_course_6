29b424cbeb5450d2276000ef770fb4bf914457de
Pavel Gornostaev <pavelgornostaeff@yandex.ru>
2024-07-23 21:52:59 +0300

    зарегистрировал админку для клиента, сообщения и пользователя

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailing.models import Client, MailingMessage
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'owner',)
+    list_filter = ('owner',)
+    search_fields = ('email', 'owner',)
+
+
+@admin.register(MailingMessage)
+class MailingMessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'subject', 'letter', 'owner',)
+    list_filter = ('subject', 'owner',)
+    search_fields = ('subject', 'owner',)
